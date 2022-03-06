b616d6d47c51740d05cabbc5f817204fdac1b860
Emil <emiloo@stud.ntnu.no>
2022-03-06 14:07:23 +0100

    Fifth commit: BattleClient

---
+++ src/main/java/Client.java
@@ -0,0 +1,43 @@
+import Units.CavalryUnit;
+import Units.CommanderUnit;
+import Units.InfantryUnit;
+import Units.RangedUnit;
+
+public class Client {
+    public Army createArmy1(){
+        Army humanArmy = new Army("Human Army");
+        for (int i = 0; i < 500; i++) {
+        humanArmy.addUnit(new InfantryUnit("Footman", 100));
+        }
+        for (int i = 0; i <200 ; i++) {
+            humanArmy.addUnit(new RangedUnit("Archer", 100));
+        }
+        for (int i = 0; i < 100 ; i++) {
+            humanArmy.addUnit(new CavalryUnit("Knight", 100));
+        }
+        humanArmy.addUnit(new CommanderUnit("Mountain King", 200));
+        return humanArmy;
+    }
+    public Army createArmy2(){
+        Army orcishHorde = new Army("Orcish Horde");
+        for (int i = 0; i < 500; i++) {
+            orcishHorde.addUnit(new InfantryUnit("Grunt", 100));
+        }
+        for (int i = 0; i <200 ; i++) {
+            orcishHorde.addUnit(new RangedUnit("Spearman", 100));
+        }
+        for (int i = 0; i < 100 ; i++) {
+            orcishHorde.addUnit(new CavalryUnit("Raider", 100));
+        }
+        orcishHorde.addUnit(new CommanderUnit("Gul'dan", 200));
+        return orcishHorde;
+    }
+    public Army simulate(){
+        Battle battle = new Battle(createArmy1(), createArmy2());
+        return battle.simulate();
+    }
+    public static void main(String[] args) {
+        Client client = new Client();
+        System.out.println(client.simulate());
+    }
+}
