7d4a449eb439159451f8176f7ba8280022829e5a
Emil <emiloo@stud.ntnu.no>
2022-04-03 22:23:09 +0200

    Save to file amd read from file update and TESTS

--- src/test/java/FileWriterTest.java
+++ src/test/java/FileWriterTest.java
@@ -1,16 +1,24 @@
 import Units.*;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class FileWriterTest {
+    ArrayList<Unit> units;
+    @BeforeEach
+    public void testData(){
+        units = new ArrayList<>();
+        units.add(new InfantryUnit("Swordman",50));
+        units.add(new CommanderUnit("Knight",50));
+        units.add(new RangedUnit("Archer",60));
+    }
 
     @Test
-    public void testSaveArmyToFile() {
-        Army army = new Army("saveToFileTestArmy");
-        army.addUnit(new InfantryUnit("Swordman",50));
-        army.addUnit(new CommanderUnit("Knight",50));
+    public void saveArmyToFile() {
+        Army army = new Army("saveToFileTestArmy", units);
         FileWriter fileWriter = new FileWriter();
         fileWriter.saveArmyToFile(army);
 
@@ -18,10 +26,19 @@ public class FileWriterTest {
         File file = new File(path);
         Assertions.assertTrue(file.isFile());
         file.delete();
+    }
+
+    @Test
+    public void saveArmyToFileThatAlreadyExists(){
+        Army army = new Army("saveToFileTestArmy", units);
+        FileWriter fileWriter = new FileWriter();
+        fileWriter.saveArmyToFile(army);
+        fileWriter.saveArmyToFile(army);
+
 
     }
     @Test
-    public void testReadArmyFromFile() {
+    public void readArmyFromFile() {
         String armyName = "readFromFileTestArmy";
        FileWriter fileWriter = new FileWriter();
        Army army = fileWriter.readArmyFromFile(armyName);
@@ -29,7 +46,7 @@ public class FileWriterTest {
     }
 
     @Test
-    public void testDeleteArmyFile() {
+    public void deleteArmyFile() {
         Army army = new Army("deleteArmyTest");
         army.addUnit(new InfantryUnit("Swordman",50));
         army.addUnit(new CommanderUnit("Knight",50));
